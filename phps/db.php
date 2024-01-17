0bf916942202c94c56de6880cd81a7ac01234c8b
Donttellmeimcute <24362190+donttellmeimcute@users.noreply.github.com>
2024-01-17 12:22:59 -0600

    Changes to be committed: 	modified:   phps/db.php

--- phps/db.php
+++ phps/db.php
@@ -4,22 +4,19 @@ $username = "root";
 $password = "toor";
 $dbname = "myDB";
 
-// Crear conexión
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificar conexión
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Recibir datos del formulario
 $nombre = $_POST['nombre'];
 $correo = $_POST['correo'];
 $telefono = $_POST['telefono'];
 $contrasena = $_POST['contrasena'];
 $nacido = $_POST['nacido'];
 
-// Insertar datos en la base de datos
 $sql = "INSERT INTO MyGuests (nombre, correo, telefono, contrasena, nacido)
 VALUES ('$nombre', '$correo', '$telefono', '$contrasena', '$nacido')";
 
