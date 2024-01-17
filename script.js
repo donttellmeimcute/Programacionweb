ea75ff0c66cb656b9ec1cc44be7ede215b81ba21
Donttellmeimcute <24362190+donttellmeimcute@users.noreply.github.com>
2024-01-17 11:07:02 -0600

    modified:   script.js 	modified:   testcommand.html

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 {
-    
+    function cargaEstrellas()
+    {
+        const bell = document.querySelector("#belleza")
+        const output = document.querySelector("#medida")
+        output.textContent = bell.value;
+        var quant = bell.value;
+        for(var i = 0;<quant;i++)
+        {
+            let image = new image();
+            let imagesrc = "imagenes/estrella.png";
+            image.src = src;
+            image.style.width?"30px";
+            document.querySelector('#medida').append(image);
+
+
+        }
+}
 }
